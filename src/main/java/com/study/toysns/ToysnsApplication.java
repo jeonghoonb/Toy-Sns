93bc9143d24e045b02b4bb66c5d10f5ea62491ee
jeonghoonb <bangdjh2@gmail.com>
2022-09-03 15:48:59 +0900

    Project Init * gitignore * gradle dependency 추가

---
+++ src/main/java/com/study/toysns/ToysnsApplication.java
@@ -0,0 +1,14 @@
+package com.study.toysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ToysnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/toysns/ToysnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.toysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
